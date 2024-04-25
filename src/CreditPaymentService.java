d40e1d4d6e5a5f49c0a628d46be22e6b8780217c
Dekarabia <Shahbanova321678@mail.ru>
2024-04-25 09:17:50 +0700

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
 
     public double calculate(double percent, int amount, int term) {
 
-        double percentM = percent / 100/  12; // ежемесячная процентная ставка
+        double percentM = percent / 100 / 12; // ежемесячная процентная ставка
         double ratio = ((percentM * Math.pow((1 + percentM), term * 12)) / (Math.pow((1 + percentM), term * 12) - 1));
         double payment = amount * ratio;
         return (int) payment;
